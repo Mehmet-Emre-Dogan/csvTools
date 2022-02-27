a9ad4ba47289e5e30d46339b221a0686320a0aeb
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-02-28 02:41:06 +0300

    download ccsv added

---
+++ downloadCsv.py
@@ -0,0 +1,35 @@
+import requests
+import datetime
+from msvcrt import getch
+
+ENCODING = "utf-8"
+PATH = "./"
+
+links = []
+
+with open("links.txt", "r", encoding="utf-8") as fil:
+    links = fil.readlines()
+    # print(links)
+
+print(f"--> {len(links)} link found. Download starting...")
+
+for i, link in enumerate(links):
+    i += 1
+    try:
+        print(f"-> {str(i).zfill(3)}: Downloading {link}")
+        content = b""
+        content = requests.get(link).content
+        # print(content)
+        customDT = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
+        fileName = customDT + "__" + link.split("/")[-1].replace("\n", "")
+        # print(fileName)
+        with open(PATH + fileName, "wb") as fil:
+            fil.write(content)
+    except Exception as ex:
+        print("An error occured:")
+        print(ex)
+    finally:
+        print(f"-> saved file: {fileName}")
+
+print("--> All links are downloaded. Press any key to exit.")
+getch()
